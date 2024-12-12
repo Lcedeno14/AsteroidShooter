a29bbf03101fb43a4d721768d0ff61e57743b5c6
Lcedeno14 <luisxcedeno@gmail.com>
2024-12-12 14:39:57 -0800

    pygame screen working

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from constants import * 
 def main():
+    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(pygame.get_init())
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+    #pygame.display.update()
 if __name__ == "__main__":
     main()
