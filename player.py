34df6b59908d08c24b0877ebfa4e64a8276bed35
Lcedeno14 <luisxcedeno@gmail.com>
2025-02-14 14:02:34 -0800

    White player triangle

--- player.py
+++ player.py
@@ -4,8 +4,11 @@ from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        rotation = 0
+        self.rotation = 0
     
+    def draw(self, screen):
+        pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
+        
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -14,5 +17,4 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def draw(self, screen):
-        pass
+    
