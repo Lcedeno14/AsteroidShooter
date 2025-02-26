2c016aba4178dd98f786565f6aec263d7014cef4
Lcedeno14 <luisxcedeno@gmail.com>
2025-02-26 11:27:12 -0800

    New updatable and drawable groups to contain different objects

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         #limit game fps to 60
         dt = clock.tick(60) /1000
