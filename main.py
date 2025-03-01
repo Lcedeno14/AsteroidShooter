48859278037ca48425f2eeb1eeeac73d7531b376
Lcedeno14 <luisxcedeno@gmail.com>
2025-02-28 17:04:05 -0800

    Asteroids are now visible and moving

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from constants import * 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     
     while (True):
         for event in pygame.event.get():
